89c88eeea2050710431da45c816d88cf387f157b
colora61292 <ps4000@hotmail.com>
2014-04-29 01:34:47 +0800

    re-commit

--- classes/model.js
+++ classes/model.js
@@ -7,6 +7,7 @@ var Model = function(model){
     this.rightColumn = false;
     this.levelOneMenuList = [];
     this.levelTwoMenuList = [];
+    this.pageContent = '';
 };
 
 //extend super class
--- models/web/main/index.js
+++ models/web/main/index.js
@@ -9,54 +9,11 @@ require('util').inherits(Model, require('classes/model'));
 
 Model.prototype.loadContent = function(callback){
 
-    /*var cms = require('classes/cms');
+    Model.super_.prototype.loadContent.apply(this, [function(err){
 
-    cms.getItems();*/
-    var this_ = this;
+        callback(err);
 
-    var formId = require('classes/global').config.formId.sitemap;
-
-    var Form = require('classes/mongoose').model('Form');
-
-    var LevelOneItem = require('classes/mongoose').model('Item_'+formId);
-
-    LevelOneItem.find({}).exec(function(err, docs){
-
-        this_.levelOneMenuList = docs;
-
-    });
-
-    Form.find({"parentItem.formId":formId}).exec(function(err, levelOneDocs){
-
-        var finishCount = levelOneDocs.length;
-
-        for(var i=0;i<levelOneDocs.length; i++){
-
-            var LevelTwoItem = require('classes/mongoose').model('Item_'+levelOneDocs[i]._id);
-
-            LevelTwoItem.find({}).exec((function(parentItem){
-
-                return function(err, levelTwoDocs){
-                    //console.log(levelTwoDocs);
-                    for(var j=0;j<levelTwoDocs.length; j++){
-
-                        //console.log(parentItem);
-                        levelTwoDocs[j].parentItem = parentItem;
-                        this_.levelTwoMenuList.push(levelTwoDocs[j]);
-
-                    }
-
-                    if(--finishCount == 0 || err != null) {
-                        //console.log(this_.levelTwoMenuList);
-                        callback(err);
-                    }
-                }
-
-            })(levelOneDocs[i].parentItem));
-        }
-
-    });
-
+    }]);
 };
 
 module.exports = Model;
